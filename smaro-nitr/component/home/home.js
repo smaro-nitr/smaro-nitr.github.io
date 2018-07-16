895e08d48f26562b59f60e9ca70643b9419d7412
Subhendu Kumar Sahoo <smaro.nitr@gmail.com>
2018-07-16 23:51:30 +0530

    Add files via upload

--- smaro-nitr/component/home/home.js
+++ smaro-nitr/component/home/home.js
@@ -1,7 +1,30 @@
 module.controller("home", home);
-function home($scope, $state) {
+function home($scope, $state, $http) {
 	this.$onInit = function() {
-		
+		$scope.codeNow();
+		$scope.quickLink();
+	};
+
+	$scope.codeNow = function() {
+		$http({
+			method : "GET",
+			url : "smaro-nitr/assets/mocker/codenow.json"
+		}).then(function mySuccess(response) {
+			$scope.codeNowData = response.data;
+		}, function myError(response) {
+			$scope.codeNowData = response.statusText;
+		});
+	};
+
+	$scope.quickLink = function() {
+		$http({
+			method : "GET",
+			url : "smaro-nitr/assets/mocker/quicklink.json"
+		}).then(function mySuccess(response) {
+			$scope.quickLinkData = response.data;
+		}, function myError(response) {
+			$scope.quickLinkData = response.statusText;
+		});
 	};
 }
-home.$inject = [ '$scope', '$state'];
+home.$inject = [ '$scope', '$state', '$http'];
