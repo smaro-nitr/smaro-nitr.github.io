9d39418fac1d93bfa2ca5beb1b8492c576cac336
Subhendu Kumar Sahoo <smaro.nitr@gmail.com>
2019-11-24 20:10:54 +0530

    Test 2

---
+++ precache-manifest.dd7e2a6558446e2b11ab5f0ad032ebb7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7495ddeee5cde7f4a6b05d1de8d62515",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f49f7e6104d2493b7324",
+    "url": "/static/css/main.cd91152e.chunk.css"
+  },
+  {
+    "revision": "147b1b6af563ccdbe15d",
+    "url": "/static/js/2.7e974e9e.chunk.js"
+  },
+  {
+    "revision": "f49f7e6104d2493b7324",
+    "url": "/static/js/main.04d0b090.chunk.js"
+  },
+  {
+    "revision": "b3f1906f011740233ed0",
+    "url": "/static/js/runtime-main.153f8a4c.js"
+  }
+]);
