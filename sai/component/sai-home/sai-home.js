0446e0b476893a512b185ac6b8dba50ea71d6c4c
Subhendu Kumar Sahoo <smaro.nitr@gmail.com>
2019-11-26 08:39:08 +0530

    v18 Push

---
+++ sai/component/sai-home/sai-home.js
@@ -0,0 +1,69 @@
+module.controller("sai-home", saiHome);
+function saiHome(saiService, $rootScope, $scope, $window, $interval, $location) {
+	this.$onInit = function() {
+		$window.scrollTo(0, 0);
+		if($scope.accessMode("apptype")=="desktopapp"){
+			$rootScope.navbarVisible = false;
+			$scope.accesstype="desktopapp";
+		}else{
+			$rootScope.navbarVisible = true;
+			$rootScope.navbarContactVisible=true;
+		}
+		$scope.loaderVisibility = true;
+		$scope.appStatus("appstatus");
+		$scope.faqDetail("faqdetail");
+		$scope.promise = $interval(function(){
+			if($window.document.readyState==="complete"){
+				$scope.loaderVisibility = false;
+				$scope.startCarousel();
+				$interval.cancel($scope.promise);
+			}
+		}, 10);
+	};
+
+	$scope.accessMode = function(param){
+		$scope.paramLength = param.length;
+		$scope.locationHash = $location.url();
+		$scope.paramValue = ($scope.locationHash.search("apptype")=="-1")?"":$scope.locationHash.substring($scope.locationHash.search("apptype")+$scope.paramLength+1, $scope.locationHash.length)
+		return $scope.paramValue;
+	};
+
+	$scope.startCarousel =  function(){
+		$(document).ready(function(){
+			$('.carousel').carousel({
+				interval: 3600,
+				pause: "false"
+			})
+		}); 
+	}
+
+	$scope.appStatus = function(fileName) {
+		saiService.fetchJsonData(fileName)
+		.then(function (response) {
+			if (response.status) {
+				$scope.errorMsg = "failed to fetch appstatus json";
+			}else{
+				$scope.appStatusData = response;
+				$scope.appStatusDataLength = $scope.appStatusData.length;
+				angular.forEach($scope.appStatusData, function(value, key) {
+					if(value.Latest==true){
+						$scope.latestMicrosoftDownloadLink = value.MicrosoftDownloadLink;
+						$scope.latestGoogleDownloadLink = value.GoogleDownloadLink;
+					};
+				});
+			}
+		});
+	};
+
+	$scope.faqDetail = function(fileName) {
+		saiService.fetchJsonData(fileName)
+		.then(function (response) {
+			if (response.status) {
+				$scope.errorMsg = "failed to fetch faqdetail json";
+			}else{
+				$scope.faqDetailData = response;
+			}
+		});
+	};
+}
+saiHome.$inject = ['saiService', '$rootScope', '$scope', '$window', '$interval', '$location'];
